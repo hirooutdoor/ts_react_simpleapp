061c817deea513b3fe83aaab15140f72f73b836d
hiro <hmaeyama0510@gmail.com>
2021-05-31 08:03:41 +0900

    create Page404 for top and under /home

--- src/router/HomeRoutes.tsx
+++ src/router/HomeRoutes.tsx
@@ -3,6 +3,7 @@
 import { Home } from "../components/pages/Home";
 import { UserManagement } from "../components/pages/UserManageMent";
 import { Setting } from "../components/pages/Setting";
+import { Page404 } from "../components/pages/Page404";
 
 export const homeRoutes = [
   {
@@ -19,5 +20,10 @@ export const homeRoutes = [
     path: "/setting",
     exact: false,
     children: <Setting />
+  },
+  {
+    path: "*",
+    exact: false,
+    children: <Page404 />
   }
 ];
